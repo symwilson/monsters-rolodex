15da11403a8bd62b6d529954006d9dd904716f1f
Simon Wilson <symwilson@yahoo.com>
2020-01-06 09:42:49 -0500

    Updates

---
+++ precache-manifest.e0e22e62bc89fa90953e65a181c1e054.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106bbf3cb8eb4f51d0ae0e4c954ab5a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5fa31cd6ff7ac77bdd65",
+    "url": "/monsters-rolodex/static/css/main.9728e0a6.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fa31cd6ff7ac77bdd65",
+    "url": "/monsters-rolodex/static/js/main.6b0dd2ea.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
